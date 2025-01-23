4b3bcbee6688378f8c2e959cf405fba5fe2dfb60
Romanov Michael <romanov.players@gmail.com>
2025-01-23 22:20:55 +0300

    add mimetypes

--- index.js
+++ index.js
@@ -1,23 +1,39 @@
 import { createServer } from 'http'
 
-function addItem() {
-
+const mimeTypes = {
+    '.html': 'text/html',
+    '.css': 'text/css',
+    '.js': 'text/javascript',
+    '.png': 'image/png',
+    '.jpg': 'image/jpeg',
+    '.gif': 'image/gif',
+    '.svg': 'image/svg+xml',
+    '.ico': 'image/x-icon',
+    '.json': 'application/json',
+    '.mp3': 'audio/mpeg',
+    '.mp4': 'video/mp4',
+    '.txt': 'text/plain',
+    '.pdf': 'application/pdf',
+    '.doc': 'application/msword',
+    '.docx': 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+    '.xls': 'application/vnd.ms-excel',
+    '.xlsx': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    'woff': 'application/font-woff',
+    'woff2': 'application/font-woff2',
+    'ttf': 'application/font-ttf',
+    'eot': 'application/vnd.ms-fontobject',
+    'otf': 'application/font-otf',
+    'swf': 'application/x-shockwave-flash',
+    'wasm': 'application/wasm'    
 }
 
 const app = createServer((request, response) => {
-    switch(req.url){
-        case 'f':
-            response.end('salam') 
-            break
-        case '/': 
-            response.end('home page')
-            break
-        default: 
-        response.statusCode = 404
-        response.end('page not found')
-        
-    }
+    const parsedUrl = new URL(request.url, 'http://localhost:4344')
+
+    console.log(parsedUrl)
+    response.end('')
 }) 
 
 app.listen('4344')
+
 
